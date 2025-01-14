a62711103cb82ea0d3743a28dde22c335daaef61
Malka <mywork0145@gmail.com>
2025-01-14 12:57:52 +0200

    Initial commit

--- app.js
+++ app.js
@@ -11,70 +11,6 @@ const outputFile = './swagger_output_institution.json';
 const endpointsFiles = ['./routes/weatherRoutes.js'];
 const swaggerAutogen = require('swagger-autogen')();
 
-///DB 
-
-const { CosmosClient } = require("@azure/cosmos");
-const { DefaultAzureCredential } = require("@azure/identity");
-const endpoint = process.env.COSMOS_ENDPOINT;
-const credential = new DefaultAzureCredential();
-
-// const cosmosClient = new CosmosClient({ 
-//     endpoint, 
-//     aadCredentials: credential
-// });
-
-// const { CosmosClient } = require('@azure/cosmos');
-// const { DefaultAzureCredential,ManagedIdentityCredential } = require("@azure/identity");
-// // const credential = new DefaultAzureCredential();
-// const credential = new ManagedIdentityCredential();
-
-
-// const cosmosClient = new CosmosClient({ 
-//     endpoint,
-//     aadCredentials: credential
-// });
-
-// connectToCosmosDB();
-
-// const databaseName = 'WeatherForecastDB';
-// cosmosClient.databases.createIfNotExists({ id: databaseName });
-
-// const {statusCode, database } =
-// console.log(`Database created with status code: ${statusCode}`);
-// console.log(`Database created with status code: ${database}`);
-
-//  const client = new CosmosClient({ connectionString });
-// const endpoint = process.env.COSMOS_ENDPOINT;
-// console.log(endpoint);
-// const key =process.env.COSMOS_KEY;
-// console.log(key);
-// const client = new CosmosClient({ endpoint, key });
-
-// const connectionString = 'https://lud.documents.azure.com:443/;AccountKey=AT4MIbEhOVQON40wqlbO1vW1M0CvCr8PRrd1psF84AcipdFvYV72shtht5fYbKVhMaIUeFEgNe3VACDb6E6Xcg==';
-
-// const client = new CosmosClient({ connectionString });
-// async function createContainer() {
-//     const database = client.database('WeatherForecastDB');
-  
-//     const containerName = 'WeatherForecast';
-
-//     // Create the container if it doesn't exist
-//     const { statusCode, container } = await database.containers.createIfNotExists({ id: containerName });
-//     console.log(`Container created with status code: ${statusCode}`);
-// }
-
-// createContainer().catch(err => {
-//     console.error(err);
-// });
-
-///DB end
-
-
-// app.use('/api-docs-institution', swaggerUi.serveFiles(institutionSwagger), swaggerUi.setup(institutionSwagger));
-//Add a route for the root URL
-// app.get('/', (req, res) => {
-//   res.send('Welcome to Weather API');
-// });
 
 //Serve the HTML file 
 process.setMaxListeners(2000); // or any other number that makes sense for your app
@@ -88,22 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add a route for the root URL 
 app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'index.html')); });
 
-
  
- 
-// swaggerAutogen(outputFile, endpointsFiles
-   
-// )
-
-
- 
-
- 
- 
-// swaggerAutogen(outputFile, endpointsFiles
-   
-// )
-
 app.use('/api', weatherRoutes);
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
